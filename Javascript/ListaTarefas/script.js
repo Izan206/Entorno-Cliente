a40711ef4c404bd584ca8dd6581194ac8000f252
Izan <alvarezvarelaizan@gmail.com>
2025-10-24 21:27:52 +0200

    .

--- Javascript/ListaTarefas/script.js
+++ Javascript/ListaTarefas/script.js
@@ -1,2 +1,19 @@
 let entradaInput = document.getElementById("entrada-tarefa");
 let listaTarefasInput = document.getElementById("lista-tarefas");
+
+let tarefas = [];
+
+function engadirTarefa() {
+  let entradaStr = entradaInput.value;
+  let data = new Date();
+  let dataStr = data.toDateString();
+  let tarefa = {
+    entradaStr: entradaObj,
+    dataStr: dataObk,
+  };
+
+  tarefas.push(tarefa);
+  renderizarTarefa();
+}
+
+function renderizarTarefa() {}
