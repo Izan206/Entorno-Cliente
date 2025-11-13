db038896bb92693e55076def5c3d1c5c1a71bf92
Izan <alvarezvarelaizan@gmail.com>
2025-11-13 22:15:06 +0100

    Ejemplo 1 realizado

--- segundoTrimestre/obxectospredefinidos/Exemplo1/script.js
+++ segundoTrimestre/obxectospredefinidos/Exemplo1/script.js
@@ -1,2 +1,41 @@
-console.log(innerHeight, "x", innerWidth)
-console.log(screenX, "x", screenY, "y")
+console.log(innerHeight, "x", innerWidth);
+console.log(screenX, "x", screenY, "y");
+alert("benvido a esta aplicacion");
+let x = confirm("Queres continuar?");
+console.log("Valor confirm", x);
+
+if (x) {
+  console.log("O usuario ha decidido continuar");
+} else {
+  console.log("O usuario a rechazado continuar");
+}
+
+let nome = prompt("Como te chamas?", "anonimo");
+console.log(nome);
+
+setTimeout(() => {
+  console.log("Esta mensaxe tarda en mostrase tres segundos");
+}, 3000);
+
+let id = setInterval(() => {
+  console.log("cada un segundo: ", new Date().toLocaleTimeString());
+}, 1000);
+
+setTimeout(() => {
+  clearInterval(id);
+  console.log("Dejara de mostrarse tras 5 segundos");
+}, 5000);
+
+let novaVentana = window.open("", "_blank", "width=400, height=300");
+
+setTimeout(() => {
+  if (!novaVentana.closed) {
+    novaVentana.document.writeln("Hola mundo despues de 5s");
+  }
+}, 5000);
+
+setTimeout(() => {
+    if (!novaVentana.closed) {
+        novaVentana.close();
+    }
+}, 7000)
